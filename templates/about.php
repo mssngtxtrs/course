db7bc02f12a499afe85459eb0e365991a0dca9ef
mssngtxtrs <missingtextures970@gmail.com>
2025-11-21 09:22:38 +0300

    Edited links to stylesheets
    Signed-off-by: mssngtxtrs <missingtextures970@gmail.com>

--- templates/about.php
+++ templates/about.php
@@ -6,7 +6,7 @@ echo <<<HERE
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>О нас :: $website_name</title>
-    <link rel="stylesheet" href="../media/css/style.css">
+    <link rel="stylesheet" href="media/css/style.css">
 </head>
 <body>
 HERE;
